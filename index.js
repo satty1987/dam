bb99d1f9525a90519e3e03107a3293a1d4521895
Satnam Singh <satnam.singh156@t-mobile.com>
2020-03-29 19:00:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const imageDir = './assets';
+const paginate = require('jw-paginate');
+
+const port = process.env.PORT || 8080;
+
+const array = []
+app.use('/assets', express.static('assets'))
+
+fs.readdirSync(imageDir).map(fileName => {
+    array.push(path.join(imageDir, fileName));
+   
+  });
+  app.get("/images", (req, res) => {
+    res.send(array);
+});
+app.get("/", (req, res) => {
+    res.sendFile(__dirname +'/index.html');
+});
+
+  app.listen(port, () => console.log(`Example app listening on port port!`))
