bc3764c83a5342a338dd552f20b46e1af071d0dc
Satnam Singh <satnam.singh156@t-mobile.com>
2020-03-30 23:02:09 +0530

    adding new ui

--- index.js
+++ index.js
@@ -3,21 +3,32 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const imageDir = './assets';
+const img = './images';
 
 const port = process.env.PORT || 8080;
 
 const array = []
 app.use('/assets', express.static('assets'))
 
+
 fs.readdirSync(imageDir).map(fileName => {
     array.push(path.join(imageDir, fileName));
-   
-  });
-  app.get("/images", (req, res) => {
-    res.send(array);
+
+});
+app.get("/images", (req, res) => {
+
+    const temp = [];
+
+    for (var i =0;i<array.length;i++) {
+        const ext = array[i].split('.').pop();
+        const name =   array[i].replace('assets/',"");
+        const path = array[i];
+        temp.push({extension : ext, name : name, path: path})
+    }
+    res.send(temp);
 });
 app.get("/", (req, res) => {
-    res.sendFile(__dirname +'/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-  app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port port!`))
