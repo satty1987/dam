81f703189e137a782aebfc81cd15cad1aaff2340
Mac Furry <satnammca@gmail.com>
2023-07-10 12:28:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
